979ad586652e09def960aabe28b584fe6a162940
lleon95 <lleon95@gmail.com>
2018-02-11 15:41:18 -0600

    ImageLoader working without boost

---
+++ LTI_Library_Image_Loader/OpenCV/CMakeFiles/imageLoader.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lleon95/Git/DIP/LTI_Library_Image_Loader/OpenCV/imageLoader.cpp" "/home/lleon95/Git/DIP/LTI_Library_Image_Loader/OpenCV/CMakeFiles/imageLoader.dir/imageLoader.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/lleon95/include/opencv"
+  "/home/lleon95/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ LTI_Library_Image_Loader/OpenCV/CMakeFiles/imageLoader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imageLoader.dir/imageLoader.o"
+  "imageLoader.pdb"
+  "imageLoader"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imageLoader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
